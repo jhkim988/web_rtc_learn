fd21afec85bcd5c2fed5f729b1c83448420c271a
jhkim988 <fccva3910@gmail.com>
2023-06-02 11:22:18 +0900

    randomHexString 구현 변경 substr -> slice 로 변경

--- src/rtc/WebRTCConnectProvider.js
+++ src/rtc/WebRTCConnectProvider.js
@@ -3,15 +3,13 @@ import { Peer } from "peerjs";
 export const WebRTCContext = React.createContext();
 
 // random hex string function
-const randomHexString = (length) => {
-  const str = Math.random().toString(16).substr(2, length);
-  return str;
-};
+const randomHexString = (length) => Math.random().toString(16).slice(2, 8);
 
 const WebRTCConnectProvider = ({ children }) => {
   const peerRef = useRef();
   const [peerId, setPeerId] = useState("");
   useEffect(() => {
+    console.log(randomHexString(4));
     if (peerRef.current) return;
     peerRef.current = new Peer(randomHexString(4));
     peerRef.current.on("open", id => {
